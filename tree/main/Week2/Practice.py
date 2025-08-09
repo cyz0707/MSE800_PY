55ae1520c48b07ef1068812dd335af4b8c08dfab
caoyongzhou <caoyongzhou0707@gmail.com>
2025-08-09 12:14:31 +1200

    Update Practice.py

--- tree/main/Week2/Practice.py
+++ tree/main/Week2/Practice.py
@@ -13,23 +13,20 @@ def getAnswer(word):
 def generateGuessBoard(puzzle):
     print(puzzle)
 
-def guessWord(word, answer, result):
-    
+def guessWord(word, answer, result): 
     guess = input("please input a letter: ")
     if len(guess) != 1 :
         guessWord(word, answer, result)
     else:
         trying = result
-
         if guess in word:
             print(answer.index(guess))
             trying[answer.index(guess)] = guess
-        
         else:
             print("Lose a life")
-        # if (word.index(guess) >= 0):
-        # print(trying, answer)
+
         if (trying == answer):
+            print("you win!")
             return
         else:
           print(trying)
@@ -42,7 +39,6 @@ def practice():
     for letter in word:
         result.append("")
     guessWord(word, answer, result)
-    # return puzzle
 
 if __name__ == "__main__":
     answer = practice()
