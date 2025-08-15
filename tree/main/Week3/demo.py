6a15f656dd8a07a56c1e6dc99c6a861224ec04e7
caoyongzhou <caoyongzhou0707@gmail.com>
2025-08-16 11:58:23 +1200

    update demo.txt

--- tree/main/Week3/demo.py
+++ tree/main/Week3/demo.py
@@ -1,9 +1,20 @@
-def main():
-    data = open('demo.txt')
-    lines = data.readlines()
+class File:
+    def __init__(self, url):
+        self.url = url
+    
+    def readFile(self):
+        data = open(self.url)
+        return data.readlines()
+    
+    def writeFile(self):
+        with open(self.url, 'w') as file:
+            file.write('hello world! line 12 3')      
 
-    with open('demo.txt', 'w') as file:
-        file.write('hello world! line 1 ')
+def main():
+    file = File('demo.txt')
+    lines = File.readFile(file)
+
+    File.writeFile(file)
 
     for line in lines:
         print(line)
