15999b2b7a6c779f87481de4b9cd8bbf1222726e
caoyongzhou <caoyongzhou0707@gmail.com>
2025-08-31 09:55:01 +1200

    add activity6

---
+++ tree/main/Week5/activity6.py
@@ -0,0 +1,32 @@
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name # public​
+        self._age = age # protected​
+        self.__grade = 'A' # private​
+        self.__address = '123 main st' # private​
+
+    def get_address(self):
+        return self.__address
+
+    def get_grade(self):
+        return self.__grade
+
+class Info(Student):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def get_age(self):
+        return self._age
+    
+    def get_name(self):
+        return self.name
+
+s = Student('Ali', 20)
+info = Info('Joe', 18)
+
+print(s.name) # accessible​
+print(s.get_address())
+print(info.get_age())
+print(info.get_name())
+print(s.get_grade()) # correct way​
