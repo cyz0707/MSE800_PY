c1224b1d58c307936561455b153806fc689acef5
caoyongzhou <caoyongzhou0707@gmail.com>
2025-09-13 12:00:28 +1200

    add week7 practice2

--- tree/main/Week7/practice2.py
+++ tree/main/Week7/practice2.py
@@ -1,33 +1,34 @@
 import sqlite3
 import time
 
-def create_connection():
-    conn = sqlite3.connect("YBCarRental.db")
-    return conn
+class Connection:
+    def __init__(self):
+        self.conn = sqlite3.connect("YBCarRental.db")
+        self.cursor = self.conn.cursor() 
 
 class UserService:
     def get_user(self, user_id):
-        conn = create_connection()
-        cursor = conn.cursor()
+        conn = Connection()
+        cursor = conn.cursor
         cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
         result = cursor.fetchone()
-        conn.close()
         return result
 
 class OrderService:
     def get_orders(self, user_id):
-        conn = create_connection()
-        cursor = conn.cursor()
+        conn = Connection()
+        cursor = conn.cursor
         cursor.execute("SELECT * FROM bookings WHERE user_id = ?", (user_id,))
         results = cursor.fetchall()
-        conn.close()
         return results
     
 def main():
+    conn = Connection()
     user_service = UserService()
     order_service = OrderService()
     
     start_time = time.process_time()
+    
     user = user_service.get_user(1)
     # print("User Details:", user)
     
@@ -36,6 +37,8 @@ def main():
 
     end_time = time.process_time()
     print(f"Execution time: {end_time - start_time} seconds")
+
+    conn.conn.close()
 
 if __name__ == '__main__':
     main()
